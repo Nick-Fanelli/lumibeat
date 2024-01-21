06a55e96d185df1719ae8214b9e1fa6d170132fa
Nick Fanelli <nickfanelli5@gmail.com>
2024-01-21 12:45:17 -0500

    feat: layout main app with css grid

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import CueList from "./CueList/CueLIst";
 import Header from "./Header/Header";
 import SplitPane from "./SplitPane/SplitPane";
 
+import './App.css'
+
 const App = () => {
 
     return (
@@ -16,6 +18,14 @@ const App = () => {
                 <div style={{width: "100%", height: "100%"}}></div>
 
             </SplitPane>
+
+            <div className="footer" style={{
+                backgroundColor: 'red',
+                width: "100%",
+                height: "50px"
+            }}>
+                <h1>Test</h1>
+            </div>
             
         </section>
 
