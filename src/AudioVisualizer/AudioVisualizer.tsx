d5d7add97912000e251c3b24899d91372978c9bd
Nick Fanelli <nickfanelli5@gmail.com>
2024-01-22 21:30:15 -0500

    feat: add regions callbacks

--- src/AudioVisualizer/AudioVisualizer.tsx
+++ src/AudioVisualizer/AudioVisualizer.tsx
@@ -6,7 +6,7 @@ import { convertFileSrc } from '@tauri-apps/api/tauri';
 
 import TimelinePlugin from 'wavesurfer.js/dist/plugins/timeline.js';
 import ZoomPlugin from 'wavesurfer.js/dist/plugins/zoom.js';
-import RegionsPlugin from 'wavesurfer.js/dist/plugins/regions.js';
+import RegionsPlugin, { Region } from 'wavesurfer.js/dist/plugins/regions.js';
 import HoverPlugin from 'wavesurfer.js/dist/plugins/hover.js';
 
 const formatTime = (seconds: number) => [seconds / 60, seconds % 60, (seconds % 1) * 1000].map((v) => `0${Math.floor(v)}`.slice(-2)).join(':');
@@ -16,6 +16,7 @@ const audioFilepath = convertFileSrc("/Users/nickfanelli/Downloads/spotifydown.c
 const AudioVisualizer = () => {
 
     const [duration, setDuration] = useState<string>("");
+    const [activeRegion, setActiveRegion] = useState<Region | undefined>(undefined);
 
     const regionsPluginRef = useRef<RegionsPlugin>(RegionsPlugin.create());
 
@@ -60,15 +61,31 @@ const AudioVisualizer = () => {
         const unsubscribeHandles: (() => void)[] = [];
 
         unsubscribeHandles.push(
-            regionsPluginRef.current.on('region-clicked', (region: any, e: MouseEvent) => {
-                console.log(region);
-                console.log('Region Clicked')
+            regionsPluginRef.current.on('region-clicked', (region: Region) => {
+
+                // region.color = "#0000ff";
+
+                // setActiveRegion((prev) => {
+
+                //     if(prev)
+                //         prev.color = "#c78f00";
+
+                //     region.color = "red";
+
+                //     return region;
+
+                // });
+
+                // console.log(region);
+                // console.log('Region Clicked')
             })
         );
 
         unsubscribeHandles.push(
-            regionsPluginRef.current.on('region-created', () => {
-                console.log('Region Created')
+            regionsPluginRef.current.on('region-created', (region) => {
+                region.on('play', () => {
+                    console.log("HEY");
+                })
             })
         );
 
@@ -81,9 +98,11 @@ const AudioVisualizer = () => {
         return () => {
             unsubscribeHandles.forEach((handle) => { handle(); });
             unsubscribeHandles.length = 0;
+
+            // visualizerRef.current?.removeEventListener('mousedown', deselectRegion);
         }
 
-    }, [regionsPluginRef.current]);
+    }, [regionsPluginRef.current, setActiveRegion]);
 
     return (
         <section id="audio-visualizer">
@@ -95,6 +114,7 @@ const AudioVisualizer = () => {
                 <div className="time">
                     <h1>{formatTime(currentTime)}</h1>
                     <h1>{duration}</h1>
+                    <h1>{activeRegion && activeRegion.start}</h1>
                 </div>
                 <button id="add-trigger" onClick={() => newTrigger(currentTime)}>New Trigger</button>
             </div>
