30845fb8a42184fe5a805917c5bfd381651b9808
Nick Fanelli <nickfanelli5@gmail.com>
2024-01-30 23:06:49 -0500

    feat: allow audio playback

--- src/AudioVisualizer/AudioVisualizer.tsx
+++ src/AudioVisualizer/AudioVisualizer.tsx
@@ -1,13 +1,12 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { lazy, useCallback, useEffect, useRef, useState } from 'react';
 // import { convertFileSrc } from '@tauri-apps/api/tauri';
 
-// const audioFilepath = convertFileSrc("/Users/nickfanelli/Downloads/spotifydown.com - American Ride.mp3");
-
 import './AudioVisualizer.css'
+import { AudioPlayer, AudioSource } from '../AudioPlayer/AudioPlayer';
 
 type VisualizerProps = {
 
-    duration: number
+    audioPlayer: AudioPlayer
 
 }
 
@@ -17,18 +16,21 @@ const Visualizer = (props: VisualizerProps) => {
 
     const [playhead, setPlayhead] = useState<number>(0);
 
-
     const onVisualizerClick = useCallback((x: number) => {
 
         const relativeClickPosition = x / visualizerRef.current!.offsetWidth;
-        const newPlayheadPosition = relativeClickPosition * props.duration;
-        const clampedPlayheadPosition = Math.min(Math.max(newPlayheadPosition, 0), props.duration);
+        const newPlayheadPosition = relativeClickPosition * props.audioPlayer.getDuration();
+        const clampedPlayheadPosition = Math.min(Math.max(newPlayheadPosition, 0), props.audioPlayer.getDuration());
 
         setPlayhead(clampedPlayheadPosition);
 
     }, [setPlayhead, visualizerRef.current]);
 
     useEffect(() => {
+
+        const playbackRefreshInterval = setInterval(() => {
+            setPlayhead(props.audioPlayer.getCurrentTime());
+        }, 100);
 
         const handleMouseUp = (e: MouseEvent) => {
 
@@ -61,7 +63,9 @@ const Visualizer = (props: VisualizerProps) => {
             visualizerRef.current?.removeEventListener('mousedown', handleOnMouseDown);
             visualizerRef.current?.removeEventListener('mousemove', handleMouseMove);
             visualizerRef.current?.removeEventListener('mousemove', handleMouseUp);
-            
+
+            clearInterval(playbackRefreshInterval);
+
         }
 
     }, [visualizerRef.current, onVisualizerClick]);
@@ -73,13 +77,15 @@ const Visualizer = (props: VisualizerProps) => {
                 <div className="waveform" ref={visualizerRef}>
 
                     <div className="playhead" style={{
-                        left: `${(playhead / props.duration) * 100}%`
+                        left: `${(playhead / props.audioPlayer.getDuration()) * 100}%`
                     }}></div>
+
+                    <div className="midline"></div>
 
                 </div>
 
                 <div className="controls">
-                    <input type="range" name="" id="" min={0} max={props.duration} value={playhead} onChange={(e) => setPlayhead(+e.target.value)} />
+                    <input type="range" name="" id="" min={0} max={props.audioPlayer.getDuration()} value={playhead} onChange={(e) => setPlayhead(+e.target.value)} />
                     <p>{playhead}</p>
                 </div>
 
@@ -91,18 +97,53 @@ const Visualizer = (props: VisualizerProps) => {
 
 const AudioVisualizer = () => {
 
+    const [audioPlayer, setAudioPlayer] = useState<AudioPlayer | null>(null);
+
+    useEffect(() => {
+
+        const loadAudio = async () => {
+
+            const source = await AudioSource.createAudioSourceFromFile("/Users/nickfanelli/Documents/DriveBy.mp3");
+            setAudioPlayer(new AudioPlayer(source));
+
+        }
+
+        loadAudio();
+
+
+    }, [setAudioPlayer]);
+
+    const playPause = useCallback(() => {
+
+        if(audioPlayer == null)
+            return;
+
+        audioPlayer.play();
+
+    }, [audioPlayer]);
+
+
     return (
-    
+
         <section id="audio-visualizer">
 
             <div className="controls">
-
+                <button onClick={() => {
+                    playPause();
+                }}>Play Pause</button>
             </div>
 
-            <Visualizer duration={120} />
+            {
+                audioPlayer == null ? 
+                <h1>Loading</h1>
+                :
+                <>
+                    <Visualizer audioPlayer={audioPlayer} />
+                </>
+            }
 
         </section>
-    
+
     );
 };
 
