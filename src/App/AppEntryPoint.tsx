a252abb95acaee3488cef03cc633b913404199dd
Nick Fanelli <nickfanelli5@gmail.com>
2024-02-14 22:12:18 -0500

    feat: add project creation system

---
+++ src/App/AppEntryPoint.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import '../index.css'
+
+import { initializeAudioSystem } from "./AudioPlayer/AudioPlayer";
+import App from "./App";
+
+initializeAudioSystem();
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+    <section id="bottom-resize-buffer" />
+  </React.StrictMode>,
+);
