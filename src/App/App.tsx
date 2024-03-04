ae9619bcdbdba1411a10c17bd02aa23b32e1e5a8
Nick Fanelli <nickfanelli5@gmail.com>
2024-03-03 22:14:50 -0500

    feat: auto set window title to project name

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -9,24 +9,44 @@ import { invoke } from "@tauri-apps/api";
 
 import { useEffect } from "react";
 import { signal } from "@preact/signals-react";
+import ProjectStruct, { deserializeProjectStruct } from "../Project/ProjectDataStructure";
+import { readTextFile } from "@tauri-apps/api/fs";
 
 interface WindowInfo {
 
-    window_uuid: string;
+    window_uuid: string,
+    show_file_path: string
 
 }
 
 const windowInfo = signal<WindowInfo | null>(null);
+const currentProject = signal<ProjectStruct | null>(null);
 
 const App = () => {
 
     useEffect(() => {
         invoke<string>('get_app_window_info').then((res) => {
             windowInfo.value = JSON.parse(res);
+
+            const showFilePath = windowInfo.value?.show_file_path;
+
+            if(showFilePath) {
+
+                readTextFile(showFilePath).then((res) => {
+                    currentProject.value = deserializeProjectStruct(res);
+
+                    invoke('set_window_title', {
+                        windowUuid: windowInfo.value?.window_uuid,
+                        title: currentProject.value.name
+                    })
+
+                });
+
+            }
         });
     }, [windowInfo])
 
-    if(windowInfo.value == null) {
+    if(windowInfo.value == null || currentProject.value == null) {
         return <h1>Loading...</h1>
     }
 
